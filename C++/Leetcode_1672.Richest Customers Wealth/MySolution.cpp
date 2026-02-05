0027648487186a491e563baed080533cfe193382
Yuze Ye <1090546352@qq.com>
2026-02-05 13:17:20 +0800

    Create MySolution.cpp

---
+++ C++/Leetcode_1672.Richest Customers Wealth/MySolution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max = 0;
+
+        for (int i = 0; i < accounts.size(); i++) {
+            int wealth = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                wealth += accounts[i][j];
+            }
+            max = max > wealth ? max : wealth;
+        }
+        return max;
+    }
+};
