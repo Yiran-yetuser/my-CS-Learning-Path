4f7273092f76fdf1f56409dada2f0403c6393c72
Yuze Ye <1090546352@qq.com>
2026-02-06 19:55:16 +0800

    Implement checkStraightLine function in MySolution.cpp

---
+++ C++/Leetcode_1232.Check If It Is a Straight Line/MySolution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int len = coordinates.size();
+        if (len == 2) {
+            return true;
+        }
+
+        for (int i = 2; i < len; i++) {
+            if ((coordinates[0][0] - coordinates[i][0]) *
+                    (coordinates[0][1] - coordinates[1][1]) !=
+                (coordinates[0][1] - coordinates[i][1]) *
+                    (coordinates[0][0] - coordinates[1][0])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
