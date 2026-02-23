82afc531ebc3098d639d0e0b0e854283a0a8afed
Yuze Ye <1090546352@qq.com>
2026-02-23 13:07:35 +0800

    Create BetterSolution.cpp

---
+++ C++/Leetcode_1461.Check If a String Contains All Binary Codes of Size K/BetterSolution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        if (s.size() < (1 << k) + k - 1) {
+            return false;
+        }
+
+        int num = stoi(s.substr(0, k), nullptr, 2);
+        unordered_set<int> exists = {num};
+        
+        for (int i = 1; i + k <= s.size(); ++i) {
+            num = (num - ((s[i - 1] - '0') << (k - 1))) * 2 + (s[i + k - 1] - '0');
+            exists.insert(num);
+        }
+        return exists.size() == (1 << k);
+    }
+};
+
+作者：力扣官方题解
+链接：https://leetcode.cn/problems/check-if-a-string-contains-all-binary-codes-of-size-k/solutions/519007/jian-cha-yi-ge-zi-fu-chuan-shi-fou-bao-h-1no1/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
