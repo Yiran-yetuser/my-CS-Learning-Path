9ddb3f4176a1a7930037c514550971818d88cd9d
Yuze Ye <1090546352@qq.com>
2026-03-06 14:43:30 +0800

    Create Solution2.cpp

---
+++ Leetcode_1784.Check if Binary String Has at Most One Segment of Ones/Solution2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        return s.find("01") == string::npos;
+    }
+};
+
+作者：灵茶山艾府
+链接：https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones/solutions/3915705/jian-dan-ti-jian-dan-zuo-pythonjavaccgoj-04j2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
