7d59ca2afb33bc16e8bf1ea146f1fb817329a6fc
Yuze Ye <yeyuze000@gmail.com>
2026-03-30 23:23:27 +0800

    Update Solution3.cpp

--- C++/Leetcode_2840.Check if Strings Can be Made Equal With Operations II/Solution3.cpp
+++ C++/Leetcode_2840.Check if Strings Can be Made Equal With Operations II/Solution3.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<array<int, 2>> a(26);
+        for (int i = 0; i < s1.size(); i++) {
+            int alpha1 = s1[i] - 'a';
+            int alpha2 = s2[i] - 'a';
+            a[alpha1][i % 2]++;
+            a[alpha2][i % 2]--;
+        }
+        for (auto v : a) {
+            if (v[0] != 0 || v[1] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
